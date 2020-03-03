d0453199a7693eb3e21995205c75d6aaf61153db
Tim Morton <tim.morton@gmail.com>
2020-03-03 14:35:26 -0800

    correct paths

--- scripts/prepare.py
+++ scripts/prepare.py
@@ -7,15 +7,13 @@ sys.path.append("..")
 from analysis.catalog import get_catalog
 
 # write ini files
-for dataset in ["gold", "all20k"]:
+for dataset in ["gold"]:  # , "all20k"]:
     catalog = get_catalog(dataset=dataset)
     results_root = Path(f"../results_{dataset}")
-    catalog.write_ini(N=2, root=results_root)
+    paths = catalog.write_ini(N=2, root=results_root)
 
     # write overall script
     with open(f"run_{dataset}_fits.sh", "w") as fout:
-        for i in catalog.df.index:
-            path = Path(f"../results_{dataset}/{str(i)[:2]}/{i}").absolute()
-            path.mkdir(parents=True, exist_ok=True)
+        for i, path in zip(catalog.df.index, paths):
             line = f"starfit {path} --binary --fehprior flat\n"
             fout.write(line)
